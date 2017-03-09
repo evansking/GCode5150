ff2c0323c556f2c2a7fcb1fb9df6a4d0d357e301
Evan King <esk79@cornell.edu>
2017-03-08 19:00:58 -0500

    removed init

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -1,40 +1,6 @@
 // Add documentation
-var SCREEN_WIDTH = window.innerWidth;
-var SCREEN_HEIGHT = window.innerHeight;
-var FLOOR = 0;
-
-var container;
-
-var camera, scene;
-
-var material, geometry;
-var renderer;
-
-var raycaster;
-var mouse;
-
-var objects = [];
-
-var red, blue;
-
 
 function init() {
-
-    container = document.getElementById('visualization');
-
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.set(0, 0, 100);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    scene = new THREE.Scene();
-
-    material = new THREE.LineBasicMaterial({color: 0xaaaaaa});
-
-    geometry = new THREE.Geometry();
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
 
 }
 
