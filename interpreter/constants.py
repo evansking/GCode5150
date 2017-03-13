0da0aaa75f0391aa943c0a553ecbe6701f4a7b03
Austin Lin <austinlin@Austins-MacBook-Pro.local>
2017-03-13 17:22:58 -0400

    added constants.py

---
+++ interpreter/constants.py
@@ -0,0 +1,12 @@
+G_descriptions = {	'0': 'Rapid Movement',
+					'1': 'Controlled Movement',
+					'4': 'Dwell / Wait',
+					'10': 'Retract',
+					'11': 'Unretract',
+					'20': 'Set units to inch',
+					'21': 'Set units to mm',
+					'28': 'Home',
+					'90': 'Absolute Positioning',
+					'91': 'Relative Positioning',
+					'92': 'Set Position to value' 
+					}
--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,27 +1,41 @@
 import re
+import constants
 
-class G_command:
+class Command:
 	'''
-	define object for a G command 
+	define object for a command 
 	'''
-	def __init__(self, number, arguments={}, line=''):
+	def __init__(self, command, arguments={}, line=''):
 		'''
 		input:
-			number: number of command, i.e. '0' in 'G0'
+			command: command type, i.e. 'G0'
 			arguments: dictionary mapping parameter type to number, i.e. X123 -> {'X':'123'}
-			line: line of Gcode, if this is given it will be parsed to get arguments
-		if neither arguments nor line are given, G-command has no arguments
+			line: line of Gcode, if this is given it will be parsed to get command and arguments.
+					This will overwrite the previous two inputs.
 
 		attributes:
-			self.number: command number
+			self.command: (string) command type
 			self.arguments: dictionary of arguments, given by input or parsed from gcode line
-		'''
+			self.letter: (string) command letter, i.e. 'G0' -> 'G'
+			self.number: (string) command number, i.e. 'G0' -> '0'
+		'''		
 		line = line.upper()
-		self.number = str(number)
 		if line:
+			self.command = self.parse_command(line)
 			self.arguments = self.parse_arguments(line)
 		else:
+			self.command = str(command)
 			self.arguments = arguments
+		self.letter, self.number = re.findall(r'([A-Z])([0-9]*)', self.command)[0]
+
+	def parse_command(self, line):
+		'''
+		input:
+			line: string of line of Gcode 
+		output:
+			string of command, i.e. 'G0'
+		'''
+		return line.split()[0]
 
 	def parse_arguments(self, line):
 		'''
@@ -29,17 +43,28 @@ class G_command:
 			line: string of line of Gcode 
 		output:
 			dictionary of arguments mapping parameter type to number, i.e. X123 -> {'X':'123'}
-		TODO: handle lines with multiple commands
 		'''
 		arguments = {}
 		args = line.split()[1:]
 		for arg in args:
 			try:
-				letter, number = re.findall(r'([A-Z])([0-9]*)', arg)[]
+				letter, number = re.findall(r'([A-Z])([0-9]*)', arg)[0]
 			except:
 				raise Exception('syntax error: arguments')
 			arguments[letter] = number
 		return arguments
+
+	def get_english(self):
+		'''
+		return the english description of this command
+		'''
+		return constants.G_descriptions[self.number]
+
+	#TODO: handle multiple commands on same line
+
+
+
+
 
 
 
