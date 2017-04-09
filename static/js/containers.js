406d9cc3852e9fd0c4ea29cf81312293d5eeb1cc
Evan King <esk79@cornell.edu>
2017-04-08 20:29:11 -0400

    templating index page

---
+++ static/js/containers.js
@@ -0,0 +1,44 @@
+/**
+ * Created by EvanKing on 4/8/17.
+ */
+
+
+
+// Function to allow for the IDE to be split into two divs and have width adjusted by drag
+// Adapted from http://jsfiddle.net/gaby/Bek9L/1779/
+function IDESetDrag() {
+    var isResizing = false,
+        lastDownX = 0;
+
+    $(function () {
+        var container = $('#container'),
+            left = $('#left_panel'),
+            right = $('#right_panel'),
+            handle = $('#drag');
+
+        handle.on('mousedown', function (e) {
+            isResizing = true;
+            lastDownX = e.clientX;
+        });
+
+        $(document).on('mousemove', function (e) {
+            // we don't want to do anything if we aren't resizing.
+            if (!isResizing)
+                return;
+
+            var offsetRight = container.width() - (e.clientX - container.offset().left);
+
+            left.css('right', offsetRight);
+            right.css('width', offsetRight);
+        }).on('mouseup', function (e) {
+            // stop resizing
+            isResizing = false;
+        });
+    });
+}
+
+
+$(document).ready(function () {
+    $('.hor-half').height(($(window).height() / 2) - ($('nav').height()));
+    IDESetDrag();
+});
